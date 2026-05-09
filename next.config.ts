673d0071ea33106601129b3231df93199309db0c
bakirkoybilsem1 <bakirkoybilsem1@gmail.com>
2026-05-10 00:26:57 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from "next";
 
-module.exports = nextConfig;
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
